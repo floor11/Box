8b4704fd2a3c3b7d05fcdcb36113ed01c3bead16
unknown <stylatone@gmail.com>
2010-05-25 22:44:31 +0200

    init Webapp

---
+++ j2ee/box-webapp/src/main/java/fr/floor11/box/web/ControllerServlet.java
@@ -0,0 +1,46 @@
+package fr.floor11.box.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControllerServlet
+ */
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ControllerServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+         PrintWriter out = response.getWriter();
+         txt(out);
+         out.close();
+	}
+
+	private void txt(PrintWriter out) {
+		out.write("testController19");
+         out.write("pouet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ j2ee/box-webapp/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+	<body>
+		<h2>The Box Application 4 !</h2>
+	</body>
+</html>
